465b09981567309d9fef55f765c9c462c6c070b5
maureen <maureen.murimi@moringaschool.com>
2022-05-07 15:15:38 +0300

    add emplooyee function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee = {name: "Nimo",streetAddress: "Nairobi",
+};
+
+let updateEmployeeWithKeyAndValue = (employeeObject, key, value) => Object.assign({}, employeeObject, { [key]: value })
+
+let destructivelyUpdateEmployeeWithKeyAndValue = (employeeObject, key, value) => {
+	employeeObject[key] = value;
+	return employeeObject;
+}
+
+let deleteFromEmployeeByKey = (employeeObject, key) => {
+	const newEmployeeObject = Object.assign({}, employeeObject);
+	delete newEmployeeObject[key];
+	return newEmployeeObject;
+}
+
+let destructivelyDeleteFromEmployeeByKey = (employeeObject, key) => {
+	delete employeeObject[key];
+	return employeeObject;
+}
